8c3a313a9f4397f55ac54cae42040a25846c40c8
alban.andrieu@nabla.mobi <alban.andrieu@nabla.mobi@001aa3a4-acb7-6b11-5561-771ade4b35b9>
2013-05-01 22:52:32 +0000

    Add administration scripts

---
+++ trunk/env/linux/run-xvbf.sh
@@ -0,0 +1,6 @@
+http://www.labelmedia.co.uk/blog/posts/setting-up-selenium-server-on-a-headless-jenkins-ci-build-machine.html
+sudo apt-get install xvfb
+
+sudo update-rc.d xvfb defaults 10
+
+to start : /etc/init.d/xvfb start
