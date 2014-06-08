4c12a61a8fbf23e3734b627e8ecf9c6dfd31dec0
alban.andrieu@nabla.mobi <alban.andrieu@nabla.mobi@001aa3a4-acb7-6b11-5561-771ade4b35b9>
2014-06-08 06:10:53 +0000

    Add cucumber tests

--- trunk/project/sample/interview/visma/gui/run.bat
+++ trunk/project/sample/interview/visma/gui/run.bat
@@ -15,9 +15,12 @@ REM OK call mvn org.codehaus.cargo:cargo-maven2-plugin:run -Psample,run-its,arq-
 
 REM TO TEST mvn clean install -Dlog4j.configuration=log4j.properties -Dlog4j.debug=true -Prun-its,arq-weld-ee-embedded -Dtest=LoanServiceITest > deploy.log 2>&1
 REM OK mvn -U -Dsurefire.useFile=false install -Psample,coverage,integration,run-its,arq-weld-ee-embedded -Djacoco.outputDir=~/project/sample/interview/visma/target -Ddatabase=derby -Dserver=jboss7x -Dwebdriver.chrome.driver=/var/lib/chromedriver -Dwebdriver.base.url=http://localhost:8180 -Dlog4j.configuration=log4j.properties -Dlog4j.debug=true -Darquillian=arq-weld-ee-embedded -Darquillian.launch=arq-weld-ee-embedded > deploy.log 2>&1
-REM TO RUN mvn clean install org.codehaus.cargo:cargo-maven2-plugin:run -Dserver=jetty9x > deploy.log 2>&1
+REM OK TO RUN mvn clean install org.codehaus.cargo:cargo-maven2-plugin:run -Dserver=jetty9x > deploy.log 2>&1
+
+TODO mvn -Dsurefire.useFile=false clean install -Psample,coverage,integration,run-its,arq-jetty-embedded -Djacoco.outputDir=~/project/sample/interview/visma/gui/target -Ddatabase=derby -Dserver=jetty7x -Dwebdriver.chrome.driver=/var/lib/chromedriver -Dwebdriver.base.url=http://localhost:9090 -Dlog4j.configuration=log4j.properties -Dlog4j.debug=true -Darquillian=arq-jetty-embedded -Darquillian.launch=arq-jetty-embedded  > install.log
+mvn clean install -Dserver=jetty9x -Dwebdriver.chrome.driver=/var/lib/chromedriver -Dwebdriver.base.url=http://localhost:9090  > install.log
 
 REM build with mvn clean install -Psample,run-its -Dserver=jetty9x
-call java -jar target/dependency/jetty-runner.jar --port 9090 target/*.war
+REM OK but not with integration test call java -jar target/dependency/jetty-runner.jar --port 9090 target/*.war
 
 pause
