a896c4210bffb15e65352a3aaa2d40aeb81e30a0
alban.andrieu@nabla.mobi <alban.andrieu@nabla.mobi@001aa3a4-acb7-6b11-5561-771ade4b35b9>
2014-06-10 14:15:41 +0000

    Add Parametize and refactoring as Page Object Model

--- trunk/project/sample/interview/visma/gui/src/test/java/com/nabla/project/visma/selenium/tests/SimpleWebDriverSTest.java
+++ trunk/project/sample/interview/visma/gui/src/test/java/com/nabla/project/visma/selenium/tests/SimpleWebDriverSTest.java
@@ -42,93 +42,24 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import com.thoughtworks.selenium.DefaultSelenium;
-import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
-
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import com.nabla.project.visma.selenium.tests.helper.SeleniumHelper;
+import com.nabla.project.visma.selenium.tests.pageobjects.LoanPage;
 
 public class SimpleWebDriverSTest
 {
-    // private static final String DEFAULT_CHROMEDRIVER = "C:\\chromedriver\\chromedriver.exe"; // "/var/lib/chromedriver"
-    // private static final String DEFAULT_FIREFOXBIN = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"; // "/usr/lib/firefox/firefox"
-    private static final String DEFAULT_CHROMEDRIVER = "/var/lib/chromedriver";                  // "C:\\chromedriver\\chromedriver.exe"
-    private static final String DEFAULT_FIREFOXBIN   = "/usr/lib/firefox/firefox";               // "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
-    private static final String DEFAULT_URL          = "http://localhost:9090";
-    private static final String PAGE_TO_LOAD_TIMEOUT = "30000";
-    private WebDriver           driver;
-    private String              baseUrl              = SimpleWebDriverSTest.DEFAULT_URL;
-    private String              chromeDriver         = SimpleWebDriverSTest.DEFAULT_CHROMEDRIVER;
-    private String              firefoxBin           = SimpleWebDriverSTest.DEFAULT_FIREFOXBIN;
 
-    // private final StringBuffer verificationErrors = new StringBuffer();
-    private DefaultSelenium     selenium;
+    private final SeleniumHelper helper = new SeleniumHelper();
 
     @Before
     public void setUp() throws Exception
     {
 
-        this.baseUrl = System.getProperty("webdriver.base.url");
+        this.helper.setUp();
 
-        if (null == this.baseUrl)
-        {
-            System.out.println("Use default webdriver.base.url");
-            this.baseUrl = SimpleWebDriverSTest.DEFAULT_URL;
-            System.setProperty("webdriver.base.url", this.baseUrl);
-        }
-        System.out.println("webdriver.base.url is : " + this.baseUrl + "\n");
-
-        this.chromeDriver = System.getProperty("webdriver.chrome.driver");
-        if (null == this.chromeDriver)
-        {
-            System.out.println("Use default webdriver.base.url");
-            this.chromeDriver = SimpleWebDriverSTest.DEFAULT_CHROMEDRIVER;
-            System.setProperty("webdriver.chrome.driver", this.chromeDriver);
-        }
-        System.out.println("webdriver.chrome.driver is : " + this.chromeDriver + "\n");
-
-        this.firefoxBin = System.getProperty("webdriver.firefox.bin");
-        if (null == this.firefoxBin)
-        {
-            System.out.println("Use default webdriver.firefox.bin");
-            this.firefoxBin = SimpleWebDriverSTest.DEFAULT_FIREFOXBIN;
-            System.setProperty("webdriver.firefox.bin", this.firefoxBin);
-        }
-        System.out.println("webdriver.firefox.bin is : " + this.firefoxBin + "\n");
-        // ProfilesIni allProfiles = new ProfilesIni();
-        // FirefoxProfile profile = allProfiles.getProfile("Selenium");
-        // FirefoxProfile profile = new FirefoxProfile();
-        // FirefoxBinary binary = new FirefoxBinary(new File(firefoxBin));
-        // driver = new FirefoxDriver(binary, profile);
-
-        this.driver = new ChromeDriver();
-        // driver = new FirefoxDriver(profile);
-        // driver = new HtmlUnitDriver(true);
-
-        // RemoteWebDriver does not implement the TakesScreenshot class
-        // if the driver does have the Capabilities to take a screenshot
-        // then Augmenter will add the TakesScreenshot methods to the instance
-        // WebDriver augmentedDriver = new Augmenter().augment(driver);
-        // File screenshot = ((TakesScreenshot) augmentedDriver).getScreenshotAs(OutputType.FILE);
-
-        this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        // driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
-        this.driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
-        // driver.manage().window().setSize(new Dimension(1920, 1080));
-        this.selenium = new WebDriverBackedSelenium(this.driver, this.baseUrl);
-
-        Thread.sleep(10000); // 10 s
-        // screenshot.
     }
 
     /*
@@ -140,48 +71,6 @@ public class SimpleWebDriverSTest
      * }
      */
 
-    @Given("the user is on Loan Page")
-    public void The_user_is_on_loan_page()
-    {
-
-        this.driver.get(this.baseUrl + "/visma/loan.xhtml");
-    }
-
-    @When("he enters \"([^\"]*)\" as loan amount")
-    public void He_enters_loan_amount(final String loanAmount)
-    {
-        this.driver.findElement(By.name("loan_form:loanAmount")).clear();
-        this.driver.findElement(By.name("loan_form:loanAmount")).sendKeys(loanAmount);
-    }
-
-    @And("he enters \"([^\"]*)\" as payback time")
-    public void He_enters_payback_time(final String paybackTime)
-    {
-        this.driver.findElement(By.name("loan_form:paybackTime")).clear();
-        this.driver.findElement(By.name("loan_form:paybackTime")).sendKeys(paybackTime);
-    }
-
-    @And("he Submits request for payments calculation")
-    public void He_submits_request_for_fund_transfer()
-    {
-        this.driver.findElement(By.id("transfer")).click();
-    }
-
-    @Then("ensure the payment schedule is accurate with \"([^\"]*)\" message")
-    public void Ensure_the_fund_transfer_is_complete(final String msg)
-    {
-        final WebElement message = this.driver.findElement(By.cssSelector("h4"));
-        Assert.assertEquals(message.getText(), msg);
-    }
-
-    @Then("ensure a transaction failure message \"([^\"]*)\" is displayed")
-    public void Ensure_a_transaction_failure_message(final int i, final String msg)
-    {
-        final WebElement message = this.driver.findElement(By.xpath("//table[@id='loan_form:panel']/tbody/tr[" + i + "]/td[3]/span"));
-        Assert.assertEquals(message.getText(), msg);
-
-    }
-
     @Test
     @InSequence(1)
     public void testWithGoodInputS() throws Exception
@@ -189,47 +78,14 @@ public class SimpleWebDriverSTest
         // Get the Start Time
         final long startTime = System.currentTimeMillis();
 
-        this.The_user_is_on_loan_page();
+        // Create an instance of Loan Page class
+        // and provide the driver
+        final LoanPage loanPage = new LoanPage();
 
-        final JavascriptExecutor js = (JavascriptExecutor) this.driver;
+        // Open the Loan Calculator Page
+        loanPage.get();
 
-        // Get the Load Event End
-        final long loadEventEnd = (Long) js.executeScript("return window.performance.timing.loadEventEnd;");
-
-        // Get the Navigation Event Start
-        final long navigationStart = (Long) js.executeScript("return window.performance.timing.navigationStart;");
-
-        // Difference between Load Event End and Navigation Event Start is Page Load Time
-        System.out.println("Page Load Time is " + ((loadEventEnd - navigationStart) / 1000) + " seconds.");
-
-        this.selenium.waitForPageToLoad(SimpleWebDriverSTest.PAGE_TO_LOAD_TIMEOUT);
-
-        // Wait for the Calculate Button
-        new WebDriverWait(this.driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.id("loan_form:payment")));
-
-        // WebElement myDynamicElement = (new WebDriverWait(driver, 20)).until(ExpectedConditions.presenceOfElementLocated(By.id("loan_form")));
-        Assert.assertEquals("Housing Loan Cost Calculator", this.driver.findElement(By.cssSelector("h3")).getText());
-        this.He_enters_loan_amount("1000000");
-        this.He_enters_payback_time("10");
-
-        // wait for the application to get fully loaded
-        final WebElement findOwnerLink = (new WebDriverWait(this.driver, 5)).until(new ExpectedCondition<WebElement>()
-        {
-            @Override
-            public WebElement apply(final WebDriver d)
-            {
-                // d.get(baseUrl);
-                return d.findElement(By.name("loan_form:paybackTime"));
-            }
-        });
-
-        findOwnerLink.click();
-
-        final WebDriverWait wait = new WebDriverWait(this.driver, 10);
-        wait.until(ExpectedConditions.elementToBeClickable(By.name("loan_form:payment")));
-        this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-        this.driver.findElement(By.name("loan_form:payment")).click();
+        loanPage.calculatePayments("1000000", "10");
 
         // Get the End Time
         final long endTime = System.currentTimeMillis();
@@ -238,13 +94,19 @@ public class SimpleWebDriverSTest
         final long totalTime = endTime - startTime;
         System.out.println("Total Page Load Time: " + totalTime + " milliseconds");
 
-        Assert.assertEquals("Housing Loan Cost Calculator (Results)", this.driver.findElement(By.cssSelector("h3")).getText());
-        this.Ensure_the_fund_transfer_is_complete("Payments total is : 1302315.33552576902309236382167649640");
-        Assert.assertEquals("10852.62779604807519243636518063747", this.driver.findElement(By.xpath("//td[2]")).getText());
+        Assert.assertEquals("Housing Loan Cost Calculator (Results)", this.helper.getDriver().findElement(By.cssSelector("h3")).getText());
+        loanPage.Ensure_the_fund_transfer_is_complete("Payments total is : 1302315.33552576902309236382167649640");
+        final WebElement simpleTable = this.helper.getDriver().findElement(By.id("payments"));
+        SeleniumHelper.testWebTable(simpleTable, 121);
+        Assert.assertEquals("10852.62779604807519243636518063747", this.helper.getDriver().findElement(By.xpath("//td[2]")).getText());
 
-        Thread.sleep(1000);
-        this.selenium.open("/visma/");
-        this.selenium.waitForPageToLoad("1500");
+        SeleniumHelper.testTakesScreenshot("testWithGoodInputS.png", this.helper.getDriver());
+        // Thread.sleep(1000);
+
+        this.helper.getSelenium().open("/visma/");
+        this.helper.getSelenium().waitForPageToLoad("1500");
+
+        loanPage.close();
     }
 
     @Test
@@ -255,60 +117,49 @@ public class SimpleWebDriverSTest
         final StopWatch pageLoad = new StopWatch();
         pageLoad.start();
 
-        this.The_user_is_on_loan_page();
-        this.selenium.waitForPageToLoad(SimpleWebDriverSTest.PAGE_TO_LOAD_TIMEOUT);
+        // Create an instance of Loan Page class
+        // and provide the driver
+        final LoanPage loanPage = new LoanPage();
 
-        // Wait for the Calculate Button
-        new WebDriverWait(this.driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.id("loan_form:payment")));
+        // Open the Loan Calculator Page
+        loanPage.get();
 
-        // Stop the StopWatch
-        pageLoad.stop();
-        System.out.println("Total Page Load Time: " + pageLoad.getTime() + " milliseconds");
-
-        // WebElement myDynamicElement = (new WebDriverWait(driver, 20)).until(ExpectedConditions.presenceOfElementLocated(By.id("loan_form")));
-        Assert.assertEquals("Housing Loan Cost Calculator", this.driver.findElement(By.cssSelector("h3")).getText());
-
-        this.He_enters_loan_amount("-10");
-        this.He_enters_payback_time("0");
+        loanPage.He_enters_loan_amount("-10");
+        loanPage.He_enters_payback_time("0");
 
         // wait for the application to get fully loaded
-        final WebElement findOwnerLink = (new WebDriverWait(this.driver, 5)).until(new ExpectedCondition<WebElement>()
-        {
-            @Override
-            public WebElement apply(final WebDriver d)
-            {
-                // d.get(baseUrl);
-                return d.findElement(By.name("loan_form:paybackTime"));
-            }
-        });
+        /*
+         * final WebElement findOwnerLink = (new WebDriverWait(this.helper.getDriver(), 5)).until(new ExpectedCondition<WebElement>()
+         * {
+         * @Override
+         * public WebElement apply(final WebDriver d)
+         * {
+         * // d.get(baseUrl);
+         * return d.findElement(By.name("loan_form:paybackTime"));
+         * }
+         * });
+         * findOwnerLink.click();
+         */
 
-        findOwnerLink.click();
-
-        final WebDriverWait wait = new WebDriverWait(this.driver, 10);
+        final WebDriverWait wait = new WebDriverWait(this.helper.getDriver(), 10);
         wait.until(ExpectedConditions.elementToBeClickable(By.name("loan_form:payment")));
-        this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        this.helper.getDriver().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-        this.driver.findElement(By.name("loan_form:payment")).click();
+        this.helper.getDriver().findElement(By.name("loan_form:payment")).click();
 
-        this.Ensure_a_transaction_failure_message(2, "Please enter the amount of your loan. Ex. 200000: Validation Error: Specified attribute is not between the expected values of 1 and 1,000,000,000.");
-        this.Ensure_a_transaction_failure_message(3, "Please enter the number of years you have to pay back your loan. Ex. 30: Validation Error: Specified attribute is not between the expected values of 1 and 120.");
+        loanPage.Ensure_a_transaction_failure_message(2, "Please enter the amount of your loan. Ex. 200000: Validation Error: Specified attribute is not between the expected values of 1 and 1,000,000,000.");
+        loanPage.Ensure_a_transaction_failure_message(3, "Please enter the number of years you have to pay back your loan. Ex. 30: Validation Error: Specified attribute is not between the expected values of 1 and 120.");
 
-        Thread.sleep(1000);
+        SeleniumHelper.testTakesScreenshot("testWithWrongInputS.png", this.helper.getDriver());
+        // Thread.sleep(1000);
 
-        this.selenium.open("/visma/");
-        this.selenium.waitForPageToLoad("1500");
+        this.helper.getSelenium().open("/visma/");
+        this.helper.getSelenium().waitForPageToLoad("1500");
     }
 
     @After
     public void tearDown() throws Exception
     {
-        this.driver.quit();
-        /*
-         * final String verificationErrorString = this.verificationErrors.toString();
-         * if (!"".equals(verificationErrorString))
-         * {
-         * Assert.fail(verificationErrorString);
-         * }
-         */
+        this.helper.tearDown();
     }
 }
