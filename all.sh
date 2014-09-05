14d051a35d226e6415eed3d08a68253fd60d6dc4
alban.andrieu <alban.andrieu@nabla.mobi>
2014-09-05 18:19:11 +0000

    Test

--- all.sh
+++ all.sh
@@ -2,9 +2,10 @@
 
 #mvn help:effective-pom 2>&1 > effective.log
 
-#aaaffdsfa
-afsafsdf
-mvn clean verify deploy -Psample,arq-jbossas-managed -Dserver=jboss5x -Ddatabase=derby 2>&1 > install.log
-afafafsafiufosfas
+#aafasaffdsfa
+aafsfsafsdf
+
+afmvn clean verify deploy -Psample,arq-jbossas-managed -Dserver=jboss5x -Ddatabase=derby 2>&1 > install.log
+affiufosfas
 
 exit 0
