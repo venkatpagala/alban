851dabfd077504524aa5428fbe79e4d0760fad41
alban.andrieu@nabla.mobi <alban.andrieu@nabla.mobi@001aa3a4-acb7-6b11-5561-771ade4b35b9>
2014-09-05 17:56:36 +0000

    Test

--- trunk/all.sh
+++ trunk/all.sh
@@ -2,6 +2,8 @@
 
 #mvn help:effective-pom 2>&1 > effective.log
 
+sdfsdf
 #mvn clean verify deploy -Psample,arq-jbossas-managed -Dserver=jboss5x -Ddatabase=derby 2>&1 > install.log
+afsfas
 
 exit 0
