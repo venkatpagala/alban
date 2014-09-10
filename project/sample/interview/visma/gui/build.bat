0ed31b00661edffc0d32f4a9d0e3f2551d862280
alban.andrieu <alban.andrieu@nabla.mobi>
2014-09-10 12:02:11 +0000

    Add gatling

--- project/sample/interview/visma/gui/build.bat
+++ project/sample/interview/visma/gui/build.bat
@@ -11,12 +11,13 @@ REM call mvn clean verify -Psample -Dserver=jetty9x -Ddatabase=derby -Dwebdriver
 REM call mvn clean verify -Psample,arq-jbossas-managed -Dserver=jboss7x -Dwebdriver.base.url=http://localhost:8180 > install.log 2>&1
 
 REM OK call mvn -U -B clean install -Dserver=jetty9x > install.log 2>&1
-REM OK call mvn -U -B clean install -Dserver=jetty9x -Dsurefire.useFile=false -Psample,coverage,integration,run-its,arq-weld-ee-embedded -Darquillian=arq-weld-ee-embedded -Darquillian.launch=arq-weld-ee-embedded > install.log 2>&1
+REM OK call mvn -U -B clean install -Dserver=jetty9x -Dsurefire.useFile=false -Psample,jacoco,integration,run-its,arq-weld-ee-embedded -Darquillian=arq-weld-ee-embedded -Darquillian.launch=arq-weld-ee-embedded > install.log 2>&1
 
 REM TODO add property -Djacoco.outputDir=/workspace/users/albandri10/project/sample/interview/visma/gui/target -Ddatabase=derby -Dlog4j.configuration=log4j.properties -Dlog4j.debug=true 
 REM DEBUG -Darquillian.debug=true
-call mvn -U -B clean install -Dserver=jboss7x -Dsurefire.useFile=false -Psample,coverage,integration,run-its,arq-weld-ee-embedded -Dwebdriver.chrome.driver=/var/lib/chromedriver -Dwebdriver.base.url=http://localhost:8180 -Dlog4j.configuration=log4j.properties -Dlog4j.debug=true -Darquillian=arq-weld-ee-embedded -Darquillian.launch=arq-weld-ee-embedded -Darquillian.debug=true > install.log 2>&1
-REM NOK call mvn -U -B clean install -Dserver=jboss7x -Dsurefire.useFile=false -Psample,coverage,integration,run-its,arq-jbossas-managed -Ddatabase=derby -Dwebdriver.chrome.driver=/var/lib/chromedriver -Dwebdriver.base.url=http://localhost:8180 -Darquillian=arq-jbossas-managed -Darquillian.launch=arq-jbossas-managed -Darquillian.debug=true > install.log 2>&1
-REM TODO mvn -U -B clean install -Dserver=jetty9x -Dsurefire.useFile=false -Psample,coverage,integration,run-its,arq-jetty-embedded -Ddatabase=derby -Dwebdriver.chrome.driver=/var/lib/chromedriver -Dwebdriver.base.url=http://localhost:9090 -Darquillian=arq-jetty-embedded -Darquillian.launch=arq-jetty-embedded  > install.log
+call mvn -U -B clean install -Dserver=jboss7x -Dsurefire.useFile=false -Psample,jacoco,integration,run-its,arq-weld-ee-embedded -Dwebdriver.chrome.driver=/var/lib/chromedriver -Dwebdriver.base.url=http://localhost:8180 -Dlog4j.configuration=log4j.properties -Dlog4j.debug=true -Darquillian=arq-weld-ee-embedded -Darquillian.launch=arq-weld-ee-embedded -Darquillian.debug=true > install.log 2>&1
+call mvn -U -B clean install -Dserver=jetty9x -Dsurefire.useFile=false -Psample,jacoco,integration,run-its,arq-jbossas-managed -Darquillian=arq-jbossas-managed -Darquillian.launch=arq-jbossas-managed > install.log 2>&1
+REM NOK call mvn -U -B clean install -Dserver=jboss7x -Dsurefire.useFile=false -Psample,jacoco,integration,run-its,arq-jbossas-managed -Ddatabase=derby -Dwebdriver.chrome.driver=/var/lib/chromedriver -Dwebdriver.base.url=http://localhost:8180 -Darquillian=arq-jbossas-managed -Darquillian.launch=arq-jbossas-managed -Darquillian.debug=true > install.log 2>&1
+REM TODO mvn -U -B clean install -Dserver=jetty9x -Dsurefire.useFile=false -Psample,jacoco,integration,run-its,arq-jetty-embedded -Ddatabase=derby -Dwebdriver.chrome.driver=/var/lib/chromedriver -Dwebdriver.base.url=http://localhost:9090 -Darquillian=arq-jetty-embedded -Darquillian.launch=arq-jetty-embedded  > install.log
 
 pause
