c92b5ecd037b9899b44d1f36468f876c742a8e65
alban.andrieu <alban.andrieu@nabla.mobi>
2015-01-24 21:49:54 +0000

    Add fronter installer

---
+++ installer/fronter-installer/src/main/resources/install.bat
@@ -0,0 +1,8 @@
+cls
+
+REM call setenv.bat
+REM set DEFAULT_JAVA_DEBUG_OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000
+REM java %DEFAULT_JAVA_DEBUG_OPTS% -jar target\izpack-tomcat-1.0-SNAPSHOT-standard.jar %*
+call java -jar VISMAInstaller.jar > install.log 2>&1
+
+pause
